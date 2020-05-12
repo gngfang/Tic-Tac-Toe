e55437d7db31667b25da58ba1f277aad9957a4ea
Gia Gie Ng Fang <jackyatc@gmail.com>
2020-05-12 11:45:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Jquery */
+
+$('#reset').on("click", function () {
+    alert("is working");
+})
+
+$('.boxes').on("click", function () {
+    alert("still need to be able to print the letter");
+})
