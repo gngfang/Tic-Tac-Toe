71880267bab5a8ed0a82b25d60010ab4c0067646
gngfang <63154900+gngfang@users.noreply.github.com>
2020-05-12 16:48:57 -0400

    added some js and changed the css and index

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 
 
 
+const grid = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+
+let player_1 = "X";
+let player_2 = "O";
 
 
 
 
+/* game functions */
 
 
 
 
-
-
-
-
-
-
-
-
-
+/* functions for listener */
 
 
 
@@ -30,6 +32,51 @@ $('#reset').on("click", function () {
     alert("is working");
 })
 
-$('.boxes').on("click", function () {
-    alert("still need to be able to print the letter");
+/* Logging the symbol into the squares */
+
+// top-left
+$('#top-left').on("click", function () {
+    console.log("top left square");
+    $('#top-left').text("X");
+
+})
+// top-middle
+$('#top-middle').on("click", function () {
+    console.log("top middle square");
+    $('#top-middle').text("X");
+})
+// top-right
+$('#top-right').on("click", function () {
+    console.log("top right square");
+    $('#top-right').text("X");
+})
+// middle-left
+$('#middle-left').on("click", function () {
+    console.log("middle left square");
+    $('#middle-left').text("X");
+})
+// middle-middle
+$('#middle-middle').on("click", function () {
+    console.log("middle middle square");
+    $('#middle-middle').text("X");
+})
+// middle-right
+$('#middle-right').on("click", function () {
+    console.log("middle right square");
+    $('#middle-right').text("X");
+})
+// bottom-left
+$('#bottom-left').on("click", function () {
+    console.log("bottom left square");
+    $('#bottom-left').text("X");
+})
+// bottom-middle
+$('#bottom-middle').on("click", function () {
+    console.log("bottom middle square");
+    $('#bottom-middle').text("X");
+})
+// bottom-right
+$('#bottom-right').on("click", function () {
+    console.log("bottom right square");
+    $('#bottom-right').text("X");
 })
