e5d32a937fa403158cd68eec8f9adaba7f138b30
gngfang <63154900+gngfang@users.noreply.github.com>
2020-05-13 10:56:29 -0400

    Chage player

--- app(2).js
+++ app(2).js
@@ -5,35 +5,61 @@
 
 let player1 = 'x';
 let player2 = 'o';
-
+let turnCount = 1;
 let game = true
 
 
-/* Array Grid */
+/* /* Array Grid */
 const grid = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    '', '', '',
+    '', '', '',
+    '', '', ''
 
 ];
 
 
+/* Container in Jquery */
+
+const $container = $('.container');
+
+$container.on("click", function (event) {
+
+    console.log("hello");
+
+    turnCount++
+
+    if (turnCount % 2 === 0) {
+
+        $(event.target).text(player1);
+        $('.playerStatus').text('Player X Turn');
+
+    } else {
+
+        $(event.target).text(player2);
+        $('.playerStatus').text('Player O Turn');
+
+    }
+
+
+})
+
+/* The grid boxes in Jquery */
+const $box1 = $('#top-left');
+const $box2 = $('#top-middle');
+const $box3 = $('#top-right');
+const $box4 = $('#middle-left');
+const $box5 = $('#middle-middle');
+const $box6 = $('#middle-right');
+const $box7 = $('#bottom-left');
+const $box8 = $('#bottom-middle');
+const $box9 = $('#bottom-right');
+
 /* Check winning status */
 
-function checkWinner() {
-    const $box1 = $('#top-left');
-    const $box2 = $('#top-middle');
-    const $box3 = $('#top-right');
-    const $box4 = $('#middle-left');
-    const $box5 = $('#middle-middle');
-    const $box6 = $('#middle-right');
-    const $box7 = $('#bottom-left');
-    const $box8 = $('#bottom-middle');
-    const $box9 = $('#bottom-right');
-    if ($box1 && $box1.text() === $box2.text() && $box1.text() === $box3.text()) {
+const checkWinner = () => {
 
-    } else if ($box1 && $box1.text() === $box2.text() && $box1.text() === $box3.text()) {
-
+    if ($box1 != '' && $box1.html() == $box2.html() && $box1.html() == $box3.html()) {
+        console.log('yes');
 
     } else if ($box4 && $box4.text() === $box5.text() && $box4.text() === $box6.text()) {
 
@@ -54,28 +80,19 @@ function checkWinner() {
 
 }
 
-
-
-checkWinner();
-
+/* Functions for Clicking */
 
 
 
 
 
+/* Player turn change */
+/* const changePlayer = () => {
+    if ()
 
 
 
-
-
-/* Change players turn */
-
-
-
-
-
-
-
+} */
 
 
 
@@ -89,3 +106,8 @@ $('#reset').on("click", function () {
 /* Boxes Interaction */
 
 
+
+/* Invoking Functions */
+
+
+checkWinner();
